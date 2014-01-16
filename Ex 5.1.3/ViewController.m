b28c0515949bde1a1612bddee53338de933f6ced
HanbinKim <hottuc@naver.com>
2014-01-16 14:42:24 +0900

    p.182 / Ex5.1.3.
    사진 보여주는겁니다.

---
+++ Ex 5.1.3/ViewController.m
@@ -0,0 +1,67 @@
+//
+//  ViewController.m
+//  Ex 5.1.3
+//
+//  Created by SDT-1 on 2014. 1. 16..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imgaeView;
+
+@end
+
+@implementation ViewController
+
+- (IBAction)takePicture:(id)sender
+{
+    if(![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"오류" message:@"카메라가지원되지않는기종" delegate:nil cancelButtonTitle:@"확인" otherButtonTitles:nil];
+        
+        [alert show];
+        return;
+    }
+    
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.delegate=self;
+    imagePicker.allowsEditing=YES;
+    imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    [self presentModalViewController:imagePicker animated:YES];
+}
+
+- (IBAction)getImage:(id)sender
+{
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc]init];
+    imagePicker.delegate =self;
+    imagePicker.allowsEditing = YES;
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    [self presentModalViewController:imagePicker animated:YES];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *editedImage = [info objectForKey:UIImagePickerControllerEditedImage];
+    UIImage *originalImage = [info objectForKey:UIImagePickerControllerEditedImage];
+    
+    UIImage *usingImage = (nil == editedImage) ? originalImage : editedImage;
+    self.imgaeView.image = usingImage;
+    
+    [picker dismissModalViewControllerAnimated:YES];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
